7abee5f2931ab466e05e1cb8df839fa4ca971655
chlrkdgml11 <chlrkdgml11@naver.com>
2023-07-21 21:39:07 +0900

    Regist Member

---
+++ jpashop/src/main/java/jpabook/jpashop/Controller/MemberController.java
@@ -0,0 +1,45 @@
+package jpabook.jpashop.Controller;
+
+import jpabook.jpashop.domain.Address;
+import jpabook.jpashop.domain.Member;
+import jpabook.jpashop.service.MemberService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+
+import javax.validation.Valid;
+
+@Controller
+@RequiredArgsConstructor
+public class MemberController {
+
+    private final MemberService memberService;
+
+    @GetMapping("/members/new")
+    public String createForm(Model model) {
+        model.addAttribute("memberForm", new MemberForm());
+        return "members/createMemberForm";
+    }
+
+//    @Valid는 MemberForm에 있는 @NotEmpty를 확인한다.
+//    BindingResult는 오류가 생기면 컨트롤러에서 팅겨버리지만 오류가 BindingResult에 담겨서 실행된다.
+    @PostMapping("/members/new")
+    public String create(@Valid MemberForm form, BindingResult result) {
+
+        if (result.hasErrors()) {
+            return "members/createMemberForm";
+        }
+
+        Address address = new Address(form.getCity(), form.getStreet(), form.getZipcode());
+
+        Member member = new Member();
+        member.setName(form.getName());
+        member.setAddress(address);
+
+        memberService.join(member);
+        return "redirect:/";
+    }
+}
